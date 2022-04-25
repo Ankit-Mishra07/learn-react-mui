6aaf06145aa50aa45a71a2fe8b0912e3d6bdc4b4
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-04-25 22:05:05 +0530

    button mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
-
-
+import {Button, styled, Typography} from '@mui/material'
+import SettingsIcon from '@mui/icons-material/Settings';
+import Add from '@mui/icons-material/Add';
 function App() {
+
+  const BlueButton = styled(Button)(({theme}) => ({
+    backgroundColor : "skyblue",
+  color : "#888",
+  margin: 5,
+  "&:hover" : {
+    backgroundColor : "lightblue"
+  },
+
+  "&:disabled" : {
+    backgroundColor : "gray",
+    color : "white"
+  }
+  }))
+
   return (
     <div>
-     Hello
+     <Button variant="text">Text</Button>
+<Button startIcon={<SettingsIcon />} variant="contained" color="otherColor" size="small">Settings</Button>
+<Button startIcon={<Add />} variant="contained" color="success" size="small">Add new post</Button>
+<Button variant="outlined" disabled>Outlined</Button>
+<Typography variant="h1" component="p">
+  it uses h1 style but its a p tag
+</Typography>;
+    <BlueButton>My Button</BlueButton>
+    <BlueButton>My another Button</BlueButton>
     </div>
   );
 }
