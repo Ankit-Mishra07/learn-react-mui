5e72b0c2d96f4108d4f3587978f0d9cfa16908d7
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-04-27 17:33:34 +0530

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,12 @@
-import {Button, styled, Typography} from '@mui/material'
-import SettingsIcon from '@mui/icons-material/Settings';
-import Add from '@mui/icons-material/Add';
+import Feed from "./components/Feed";
+import Sidebar from "./components/Sidebar";
+import Rightbar from "./components/Rightbar";
 function App() {
-  const BlueButton = styled(Button)(({theme}) => ({
-    backgroundColor : "skyblue",
-  color : "#888",
-  margin: 5,
-  "&:hover" : {
-    backgroundColor : "lightblue"
-  },
-
-  "&:disabled" : {
-    backgroundColor : "gray",
-    color : "white"
-  }
-  }))
-
-
   return (
     <div>
-     <Button variant="text">Text</Button>
-<Button startIcon={<SettingsIcon />} variant="contained" color="otherColor" size="small">Settings</Button>
-<Button startIcon={<Add />} variant="contained" color="success" size="small">Add new post</Button>
-<Button variant="outlined" disabled>Outlined</Button>
-<Typography variant="h1" component="p">
-  it uses h1 style but its a p tag
-</Typography>;
-    <BlueButton>My Button</BlueButton>
-    <BlueButton>My another Button</BlueButton>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
     </div>
   );
 }
