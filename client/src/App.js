6177b07eb99d4e2ef3fd07b6e4742886e630dd3e
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-04-30 15:42:54 +0530

    learning navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import Feed from "./components/Feed";
 import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
+import {Box, Stack} from '@mui/material'
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div>
-      <Sidebar />
-      <Feed />
-      <Rightbar />
-    </div>
+    <Box>
+      {/* navbar */}
+      <Stack direction="row" 
+      spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import {AppBar} from "@mui/material"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <AppBar>
+    Navbar
+    </AppBar>
+    </>
   )
 }
 
